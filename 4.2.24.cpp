54217eabfd6db9be61ae08177ac2e8ed73ec4683
Kirill Sudnikovich <0311snitch@gmail.com>
2017-03-20 16:07:40 +0300

    Add files via upload

--- 4.2.24.cpp
+++ 4.2.24.cpp
@@ -1,4 +1,4 @@
-// ConsoleApplication5.cpp: определяет точку входа для консольного приложения.
+// ConsoleApplication6.cpp: определяет точку входа для консольного приложения.
 //
 
 #include "stdafx.h"
@@ -10,39 +10,65 @@
 using namespace std;
 
 string with;
+string s1;
+
+int check(string Sbuff);
 
 int main()
 {
+	setlocale(0, "rus");
 	char s[10];
 	cout << "Enter a word: ";
 	cin.getline(s, 10);
-	cout << s << endl;
-	string s1 = s;
-	for (int i = 0; s1[i] != '\0'; i++)
+	s1 = s;
+	for (int i = 0; i != 1;)
 	{
 		char buff[15];
 		string comma = ", ";
 		ifstream fin("dictionary.txt");
 		while (1)
 		{
+			int sovpad;
 			fin >> buff;
 			string Sbuff = buff;
-			for (int j = 0; Sbuff[j] != '\0'; j++)
+			for (int k = 0; Sbuff[k] != '\0'; k++)
 			{
-				if (s1[i] == Sbuff[j])
+				if (s1[i] == Sbuff[k])
 				{
-					with.append(Sbuff);
-					with.append(comma);
+					sovpad = check(Sbuff);
 				}
 			}
 			if (!buff[0])
 			{
+				i = 1;
+				break;
+			}
+			if ((sovpad == strlen(s)) && (sovpad == strlen(buff)))
+			{
+				with.append(Sbuff);
+				with.append(comma);
+			}
+		}
+	}
+	cout << "Слова, полностью совпадающие с заданным словом по составу : \n" << with;
+	system("\npause");
+	return 0;
+}
+
+int check(string Sbuff)
+{
+	int sovpad = 0;
+	for (int i = 0; s1[i] != '\0'; i++)
+	{
+		for (int k = 0; Sbuff[k] != '\0'; k++)
+		{
+			if (s1[i] == Sbuff[k])
+			{
+				sovpad++;
+				Sbuff[k] = '1';
 				break;
 			}
 		}
-		cout << "Words with letter " << s1[i] << " : " << with << "\n\n";
-		with.clear();
 	}
-	system("pause");
-	return 0;
+	return sovpad;
 }
